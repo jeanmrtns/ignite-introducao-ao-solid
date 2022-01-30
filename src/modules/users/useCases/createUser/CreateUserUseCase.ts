5cd6b158d3a523f36e3d4c71b47dde1d84a558e6
Jean Martins <jeanmrtns4@gmail.com>
2022-01-30 05:49:09 -0300

    Desafio

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (!userAlreadyExists) {
+      const user = this.usersRepository.create({ name, email });
+
+      return user;
+    }
+
+    throw new Error("Mensagem do erro");
   }
 }
 
